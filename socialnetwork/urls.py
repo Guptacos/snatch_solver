d88066ec5088ab9305df831621194480a9a59a35
Niko Gupta <niko.gupta@gmail.com>
2020-04-12 02:31:30 -0400

    Update Django code to work with prior optimization
    Since the input to getPossibleSteals changed a bit, this commit changes
    some code in views.py to maintain a global dict.

--- socialnetwork/urls.py
+++ socialnetwork/urls.py
@@ -19,5 +19,4 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('<str:word>', views.home, name='home'),
-    path('<str:word>/<str:letters>', views.home, name='home'),
 ]
