c87f1d0c6c1b3dc625745fa87ac68a5416bd7dd4
Niko Gupta <niko.gupta@gmail.com>
2020-04-12 16:56:23 -0400

    Reformat getPossibleSteals to take in letters
    If no letters passed in, then works as before and returns all possible
    steals. If letters are given, limits output to words that can be stolen
    using those letters.

--- solver_app/word_lib.py
+++ solver_app/word_lib.py
@@ -27,6 +27,9 @@ def getDict():
 # @param wordDict: the dict created by getDict. Pass in instead of recomputing.
 # @param word: the word you're trying to steal.
 # @param length: the max length of new words to look for.
+# @param letters: letters on the board as a string.
+#                 if None, assumes all letters available
+#
 # @note: longest word in the english language is ~46 characters?
 #        I have never seen a word longer than ~10-12 in snatch
 # @note: this function is basically cheating, and shouldn't be used except for
@@ -34,91 +37,40 @@ def getDict():
 #
 # @return: a list of tuples, where tup[0] is a stealable word, and tup[1] is
 #          the letters needed to steal it.
-def getPossibleSteals(wordDict, word, maxLen=12):
-    ogWord = LibWord(word)
-    result = []
+def getPossibleSteals(wordDict, word, letters=None, maxLen=12):
+    # Helper function assumes you have unlimited letters
+    def stealHelp():
+        print(maxLen)
+        ogWord = LibWord(word)
+        result = []
 
-    for elemLen, wordList in wordDict.items():
-        # Skip words that are too long or short
-        if elemLen > maxLen or elemLen <= ogWord.len:
-            continue
-
-        for dictWord in wordList:
-            if dictWord.sameRootAs(ogWord):
+        for elemLen, wordList in wordDict.items():
+            # Skip words that are too long or short
+            if elemLen > maxLen or elemLen <= ogWord.len:
                 continue
 
-            if ogWord.isSubsetOf(dictWord):
-                result.append((dictWord.text, dictWord.letterDiff(ogWord)))
+            for dictWord in wordList:
+                if dictWord.sameRootAs(ogWord):
+                    continue
 
-    result.sort(key=lambda elem: len(elem[0]))
-    return result
+                if ogWord.isSubsetOf(dictWord):
+                    result.append((dictWord.text, dictWord.letterDiff(ogWord)))
 
-# @param letters: a string to permute
-# @return: a list of strings, containing every permutation of the input (order
-#          independent)
-#          eg: input aba, output [a, b, aa, ab, aab]
-# @note: this is bad and inneficient. I don't expect inputs > 5, so largest
-#        result size = 2^5 = 32, max recursion depth = 5
-def permute(letters):
-    if letters is None or len(letters) == 0:
-        return []
+        result.sort(key=lambda elem: len(elem[0]))
+        return result
 
-    if len(letters) == 1:
-        return [letters]
+    if letters is None:
+        return stealHelp()
 
-    first = letters[0]
-    rest = permute(letters[1:])
-
+    # Constrain stealHelp's results given input letters
+    maxLen = len(word + letters)
+    possibleSteals = stealHelp()
     result = []
-    for elem in rest:
-        result += [elem]
-        result += [elem + first]
+    ogLetters = LibWord(letters)
 
-    result += [first]
-
-    return result
-
-# @param ogWord: a string containing the original word
-# @param letter: a character, returns what permutations of ogWord + letter are
-#                new words
-# @return set of new words that can be made, empty if none can be made
-'''
-def newWords(ogWord, letter):
-    dictionary = getDict()
-
-    # Count letters in original word
-    ogWordCount = Counter(ogWord + letter)
-    stealable = set()
-    for dictWord in dictionary:
-        # Count letters in dict word. If same as original, add to result
-        if ogWordCount == Counter(dictWord):
-            stealable.add(dictWord)
-
-    # Check which of the new words have a different
-    result = set()
-    for newWord in stealable:
-        if differentRoot(newWord, ogWord):
-            result.add(newWord)
+    # Only keep the word if what you need is a subset of letters
+    for stealWord, stealLetters in possibleSteals:
+        if LibWord(stealLetters).isSubsetOf(ogLetters):
+            result.append((stealWord, stealLetters))
 
     return result
-'''
-
-# Equivalent to newWords, but considers combinations of multiple letters
-#
-# @param word: the word being stolen
-# @param letters: the letters available in the middle of the board as a string
-# @return a set containing every word that can be made
-'''
-def newWordsManyLetters(wordDict, word, letters):
-    maxLen = len(word) + len(letters)
-    possibleSteals = getPossibleSteals(wordDict, word, maxLen)
-
-    permutations = permute(letters)
-
-    result = []
-    for elem in permutations:
-        if
-        result = result.union(newWords(word, elem))
-
-    return result
-'''
